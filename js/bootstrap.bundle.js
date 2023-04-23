10dc9a74c7744899647c2d17e619937a3da04404
Kalp1774 <117701948+Kalp1774@users.noreply.github.com>
2023-04-23 17:38:23 +0530

    Login webpage with loading animation

---
+++ js/bootstrap.bundle.js
@@ -0,0 +1,14 @@
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+
+    // Making 5s delay in website loading
+    setTimeout(() => {
+        loader.classList.add('fadeOut');
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 1000);
+
+    }, 5000);
+
+});
